12024365d65cfabb941fa2319fd645eedd227338
Sharif Olorin <sio@tesser.org>
2014-03-18 12:19:48 +1100

    Support reading multiple bursts and outputting a single burst

--- framefelid/framefelid.go
+++ framefelid/framefelid.go
@@ -11,6 +11,15 @@ import (
 	"os"
 )
 
+func catBurst(frames []*dataframe.DataFrame) {
+	burst := dataframe.BuildDataBurst(frames)
+	b, err := dataframe.MarshalDataBurst(burst)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error marshalling burst: %v\n", err)
+	}
+	os.Stdout.Write(b)
+}
+
 func catFrame(frame *dataframe.DataFrame, json bool) {
 	if !json {
 		fmt.Println(*frame)
@@ -18,18 +27,48 @@ func catFrame(frame *dataframe.DataFrame, json bool) {
 		writer := io.Writer(os.Stdout)
 		jsonWriter, err := framestore.NewJSONWriter(writer)
 		if err != nil {
-			fmt.Printf("Error JSON-encoding frame: %v\n\n%v\n", err, *frame)
+			fmt.Fprintf(os.Stderr, "Error JSON-encoding frame: %v\n\n%v\n", err, *frame)
 		}
 		jsonWriter.WriteFrame(frame)
 	}
 }
 
-func main() {
+func catFromStdin(json, burstPacked bool) {
 	var err error
 	var buf bytes.Buffer
+	if burstPacked {
+		_, err = buf.ReadFrom(os.Stdin)
+		if err != nil {
+			fmt.Println(err)
+		}
+		burst, err := dataframe.UnmarshalDataBurst(buf.Bytes())
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error unmarshalling databurst: %v\n", err)
+		}
+		for _, frame := range burst.Frames {
+			catFrame(frame, json)
+		}
+	} else {
+		reader := io.Reader(os.Stdin)
+		packet, err := ioutil.ReadAll(reader)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		frame, err := dataframe.UnmarshalFrame(packet)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		catFrame(frame, json)
+		if err != io.EOF {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
 
+func main() {
 	burstPacked := flag.Bool("burst", true, "Parse DataBurst-encapsulated frames rather than plain DataFrames")
 	json := flag.Bool("json", false, "Output frames in JSON")
+	pb := flag.Bool("pb", false, "Output frames as a DataBurst")
 
 	flag.Usage = func() {
 		helpMessage := "framecat will accept a stream of DataFrames on stdin and print a textual representation to stdout." +
@@ -39,31 +78,33 @@ func main() {
 		flag.PrintDefaults()
 	}
 	flag.Parse()
-	if *burstPacked {
-		_, err = buf.ReadFrom(os.Stdin)
-		if err != nil {
-			fmt.Println(err)
-		}
-		burst, err := dataframe.UnmarshalDataBurst(buf.Bytes())
-		if err != nil {
-			fmt.Println(err)
-		}
-		for _, frame := range burst.Frames {
-			catFrame(frame, *json)
-		}
+	if flag.NArg() == 0 {
+		catFromStdin(*json, *burstPacked)
 	} else {
-		reader := io.Reader(os.Stdin)
-		packet, err := ioutil.ReadAll(reader)
-		if err != nil {
-			fmt.Println(err)
+		var buf bytes.Buffer
+		frames := make([]*dataframe.DataFrame, 0)
+		for _, fname := range flag.Args() {
+			fi, err := os.Open(fname)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
+				os.Exit(1)
+			}
+			_, err = buf.ReadFrom(fi)
+			burst, err := dataframe.UnmarshalDataBurst(buf.Bytes())
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error unmarshalling burst: %v\n", err)
+				continue
+			}
+			for _, f := range burst.Frames {
+				frames = append(frames, f)
+			}
 		}
-		frame, err := dataframe.UnmarshalFrame(packet)
-		if err != nil {
-			fmt.Println(err)
-		}
-		catFrame(frame, *json)
-		if err != io.EOF {
-			fmt.Println(err)
+		if *pb {
+			catBurst(frames)
+		} else {
+			for _, f := range frames {
+				catFrame(f, *json)
+			}
 		}
 	}
 }
